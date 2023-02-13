adfd97cbce6e25e4bb371f8bf5a3e984bc8424f5
Benjamin Abarzua <abarzuabenjamin@gmail.com>
2023-02-13 15:01:10 -0300

    Add query methods call for debugging

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 /* App should use a single object that encompases 
 all the CLI input,
-  - ticketType
-  - orgName
-  - orgOwner
-  - contractStartDate
-  - seatsAmount
+  - ticketType x
+  - orgName x
+  - orgOwner x
+  - contractStartDate x
+  - seatsAmount x
 
 the methods for querying bt and BF API and the useful data received from them
   btAccount{
-      owners array
+      owners array x
     }
   btSubscriptionList{
     name(currentSub),
-    pricing_components[0].value (seat amount)
+    pricing_components[0].value (seat amount / currentSeats)
     current_period_end
   }
   getBfAccId{
-    BillForward ID (ACC-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX)
+    BillForward ID (ACC-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX) x
   }
   getSubId
     active BF subscription ID
@@ -26,6 +26,7 @@ the methods for querying bt and BF API and the useful data received from them
 const { program } = require('commander')
 const { btAccount, btSubscription, getBfAccId } = require('./btCalls')
 const { getSubId } = require('./apiCalls')
+const Query = require('./class')
 
 require('dotenv').config({
   path: __dirname + '/.env'
@@ -36,6 +37,7 @@ const BF_TOKEN = process.env.BILLFORWARD_TOKEN
 // command line arguments
 program
   .arguments('<org>')
+  .option('-t, --type <ticketType>', 'new, upsell or renewal')
   .option('-o, --owner <owner>', 'Docker ID for the owner')
   .option('-d, --date <yyyy-mm-dd>', 'Contract start date')
   .option('-s, --seats <amount>', 'Amount of seats to provision')
@@ -51,6 +53,9 @@ program.parse(process.argv)
 
 
 // User Input
+const mainQuery = new Query(program.opts().type, program.args, program.opts().owner, program.opts().date, program.opts().seats)
+
+
 const orgName = program.args
 const ownerName = program.opts().owner
 const contractStartDate = program.opts().date
@@ -101,7 +106,7 @@ async function makeTemplate() {
   if (currentSub.length == 0) {
     console.log('no active subscriptions');
   } else {
-    console.log(`canceled ${currentSub} (${currentSeats} seats) subscription: ${subUrl}`);    
+    console.log(`canceled ${currentSub} (${currentSeats} seats) subscription: ${subUrl}`);
   }
   console.log(`provisioned Docker Business - Annual (${provisionSeats} seats) subscription: `);
   console.log(' ');
@@ -112,3 +117,10 @@ async function makeTemplate() {
 }
 
 makeTemplate()
+Promise.all([
+mainQuery.btAccount(),
+mainQuery.getBfAccId(),
+mainQuery.btSubscription()])
+.then(() => mainQuery.getActiveSubId())
+.then(()=> console.log('mainQuery: ', mainQuery))
+
